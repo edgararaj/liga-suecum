aa62faca5b0b9a81969bba719d7a09156df267be
tuai-offline <tuai-offline@proton.me>
2025-03-24 23:19:05 +0000

    Small frontend fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt">
+      <body className="min-h-screen bg-gradient-to-b from-white to-slate-100 dark:from-slate-900 dark:to-slate-950">
+        {children}
+      </body>
     </html>
   )
 }
