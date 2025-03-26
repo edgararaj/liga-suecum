2e66033a476241ad92d8de49974bcd1074b5abdd
tuai-offline <tuai-offline@proton.me>
2025-03-26 10:17:45 +0000

    smol fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Liga Sueca UMinho',
+  description: 'Liga Sueca UMinho',
 }
 
 export default function RootLayout({
